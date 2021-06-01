be008be9c31b74eb2b1b5175800b36860b5d2612
charleshsc <1041807416@qq.com>
2021-06-01 16:04:00 +0800

    modify the loss function to avoid nan

--- utils/loss.py
+++ utils/loss.py
@@ -12,5 +12,9 @@ class cross_entropy(nn.Module):
         assert res_prob.size() == gt_prob.size() and len(res_prob.size()) == 2
         loss = 0
         for single_res, single_gt in zip(res_prob, gt_prob):
+            ones = torch.ones_like(single_res) - 1e-6
+            zeros = torch.zeros_like(single_res) + 1e-6
+            single_res = torch.where(single_res >= 1, ones, single_res)
+            single_res = torch.where(single_res <= 0, zeros, single_res)
             loss = loss + -torch.sum(single_gt * torch.log(single_res) + (1 - single_gt) * torch.log(1 - single_res))
         return loss/res_prob.size()[0]
